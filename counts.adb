993e73d0d2f428ecfa0fd569e3184e651568f51d
Johannes Kanig <kanig@adacore.com>
2026-01-28 11:01:03 +0900

    second lemma, unproved

--- counts.adb
+++ counts.adb
@@ -9,6 +9,37 @@ package body Counts with SPARK_Mode is
        (for all C in Character'First .. Up_To => Arr (C) = 0),
      Post               => Sum (Arr, Up_To) = 0;
 
+   function Incr (Arr : Counts_Array; Pos : Character) return Counts_Array
+   is (Arr with delta Pos => Arr (Pos) + 1)
+   with
+     Ghost,
+     Pre  => Arr (Pos) < Length'Last,
+     Post =>
+       (for all C in Character =>
+          (if C = Pos
+           then Incr'Result (C) = Arr (C) + 1
+           else Incr'Result (C) = Arr (C)));
+
+   procedure Lem_Incr_Eq (Arr : Counts_Array; Up_To, Pos : Character)
+   with
+     Ghost,
+     Global             => null,
+     Subprogram_Variant => (Decreases => Up_To),
+     Pre                => Pos > Up_To and then Arr (Pos) < Length'Last,
+     Post               => Sum (Incr (Arr, Pos), Up_To) = Sum (Arr, Up_To);
+
+   procedure Lem_Incr_Neq (Arr : Counts_Array; Up_To, Pos : Character)
+   with
+     Ghost,
+     Global             => null,
+     Subprogram_Variant => (Decreases => Up_To),
+     Pre                => Pos <= Up_To and then Arr (Pos) < Length'Last,
+     Post               => Sum (Incr (Arr, Pos), Up_To) = Sum (Arr, Up_To) + 1;
+
+   ------------------
+   -- Lem_Sum_Zero --
+   ------------------
+
    procedure Lem_Sum_Zero (Arr : Counts_Array; Up_To : Character) is
    begin
       if Up_To = Character'First then
@@ -16,8 +47,43 @@ package body Counts with SPARK_Mode is
       else
          Lem_Sum_Zero (Arr, Character'Pred (Up_To));
       end if;
-
    end Lem_Sum_Zero;
+
+   -----------------
+   -- Lem_Incr_Eq --
+   -----------------
+
+   procedure Lem_Incr_Eq (Arr : Counts_Array; Up_To, Pos : Character) is
+   begin
+      if Up_To = Character'First then
+         return;
+      else
+         Lem_Incr_Eq (Arr, Character'Pred (Up_To), Pos);
+      end if;
+   end Lem_Incr_Eq;
+
+
+   ------------------
+   -- Lem_Incr_Neq --
+   ------------------
+
+   procedure Lem_Incr_Neq (Arr : Counts_Array; Up_To, Pos : Character) is
+      Tmp : Counts_Array := Incr (Arr, Pos);
+   begin
+      if Up_To = Pos then
+         if Up_To = Character'First then
+            return;
+         else
+            Lem_Incr_Eq (Arr, Character'Pred (Up_To), Pos);
+         end if;
+      else
+         Lem_Incr_Neq (Arr, Character'Pred (Up_To), Pos);
+      end if;
+   end Lem_Incr_Neq;
+
+   -----------------
+   -- Char_Counts --
+   -----------------
 
    function Char_Counts (Input : Buffer) return Counts_Array is
       Counts : Counts_Array := [others => 0];
@@ -28,6 +94,7 @@ package body Counts with SPARK_Mode is
            Loop_Invariant
              (for all C in Character => Counts (C) <= I - Input'First);
          pragma Loop_Invariant (Sum (Counts) = I - Input'First);
+         Lem_Incr_Neq (Counts, Character'Last, Input (I));
          Counts (Input (I)) := Counts (Input (I)) + 1;
       end loop;
       return Counts;
